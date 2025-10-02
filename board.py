0524a2521b84075b5065877449507beb1e874b37
sdarbane <darbane.1998@gmail.com>
2025-10-02 12:13:09 +0200

    adding comments

--- board.py
+++ board.py
@@ -9,51 +9,57 @@
 # Every cell in the above list contains either a 0 or a 1. Player 1 is represented by 0 tiles, and Player
 
 ##############################################################################
+
+# Classe représentant le plateau de jeu de Puissance 4
 class Board(object):
 
     #static class variables - shared across all instances
+    # Constantes pour la hauteur et la largeur du plateau
     HEIGHT = 6
     WIDTH = 8
 
-    def __init__(self, orig=None, hash=None):
 
-        # copy
+    def __init__(self, orig=None, hash=None):
+        """
+        Initialise un plateau de jeu.
+        - Si 'orig' est fourni, effectue une copie du plateau existant.
+        - Si 'hash' est fourni, reconstruit le plateau à partir d'un nombre unique (base 3).
+        - Sinon, crée un plateau vide.
+        """
+        # Copie d'un plateau existant
         if(orig):
-            self.board = [list(col) for col in orig.board]
-            self.numMoves = orig.numMoves
-            self.lastMove = orig.lastMove
+            self.board = [list(col) for col in orig.board]  # Copie profonde des colonnes
+            self.numMoves = orig.numMoves  # Nombre de coups déjà joués
+            self.lastMove = orig.lastMove  # Dernier coup joué
             return
 
-        # creates from hash - NOTE: Does not understand move order
+        # Création à partir d'un hash (représentation unique du plateau)
         elif(hash):
             self.board = []
             self.numMoves = 0
             self.lastMove = None
 
-            # convert to base 3
+            # Conversion du hash en base 3 pour reconstruire le plateau
             digits = []
             while hash:
                 digits.append(int(hash % 3))
                 hash //= 3
 
             col = []
-            
             for item in digits:
-                
-                # 2 indicates new column
+                # 2 indique la fin d'une colonne
                 if item == 2:
                     self.board.append(col)
                     col = []
-                
-                # otherwise directly append base number
                 else:
+                    # Ajoute le pion à la colonne
                     col.append(item)
                     self.numMoves += 1
             return
 
-        # create new
+        # Création d'un plateau vide
         else:
-            self.board = [[] for x in range(self.WIDTH)]
+            self.board = [[] for x in range(self.WIDTH)]  # Liste de colonnes vides
             self.numMoves = 0
             self.lastMove = None
             return
@@ -63,96 +69,100 @@ class Board(object):
     #                           Mutations
     ########################################################################
 
-    # Puts a piece in the appropriate column and checks to see if it was a winning move
-    # Pieces are either 1 or 0; automatically decided
+
+    # Place un pion dans la colonne spécifiée
+    # Le joueur est déterminé automatiquement (0 ou 1)
     def makeMove(self, column):
-        # update board data
-        piece = self.numMoves % 2
-        self.lastMove = (piece, column)
-        self.numMoves += 1
-        self.board[column].append(piece)
+        piece = self.numMoves % 2  # 0 pour le joueur 1, 1 pour le joueur 2
+        self.lastMove = (piece, column)  # Mémorise le dernier coup
+        self.numMoves += 1  # Incrémente le nombre de coups
+        self.board[column].append(piece)  # Ajoute le pion dans la colonne
 
 
     ########################################################################
     #                           Observations
     ########################################################################
 
-    # Generates a list of the valid children of the board
-    # A child is of the form (move_to_make_child, child_object)
+
+    # Génère la liste des plateaux enfants valides (tous les coups possibles)
+    # Retourne une liste de tuples (colonne, nouvel objet Board)
     def children(self):
         children = []
         for i in range(7):
-            if len(self.board[i]) < 6:
-                child = Board(self)
-                child.makeMove(i)
+            if len(self.board[i]) < 6:  # Si la colonne n'est pas pleine
+                child = Board(self)  # Copie du plateau actuel
+                child.makeMove(i)    # Joue le coup dans la colonne i
                 children.append((i, child))
         return children
 
-    # Returns:
-    #  -1 if game is not over
-    #   0 if the game is a draw
-    #   1 if player 1 wins
-    #   2 if player 2 wins
+
+    # Vérifie si la partie est terminée
+    # Retourne :
+    #  -1 si la partie continue
+    #   0 si match nul
+    #   1 si le joueur 1 gagne
+    #   2 si le joueur 2 gagne
     def isTerminal(self):
-        for i in range(0,self.WIDTH):
-            for j in range(0,self.HEIGHT):
+        for i in range(0, self.WIDTH):
+            for j in range(0, self.HEIGHT):
                 try:
-                    if self.board[i][j]  == self.board[i+1][j] == self.board[i+2][j] == self.board[i+3][j]:
+                    # Vérifie 4 pions alignés horizontalement
+                    if self.board[i][j] == self.board[i+1][j] == self.board[i+2][j] == self.board[i+3][j]:
                         return self.board[i][j] + 1
                 except IndexError:
                     pass
 
                 try:
-                    if self.board[i][j]  == self.board[i][j+1] == self.board[i][j+2] == self.board[i][j+3]:
+                    # Vérifie 4 pions alignés verticalement
+                    if self.board[i][j] == self.board[i][j+1] == self.board[i][j+2] == self.board[i][j+3]:
                         return self.board[i][j] + 1
                 except IndexError:
                     pass
 
                 try:
+                    # Vérifie 4 pions alignés en diagonale descendante
                     if not j + 3 > self.HEIGHT and self.board[i][j] == self.board[i+1][j + 1] == self.board[i+2][j + 2] == self.board[i+3][j + 3]:
                         return self.board[i][j] + 1
                 except IndexError:
                     pass
 
                 try:
+                    # Vérifie 4 pions alignés en diagonale montante
                     if not j - 3 < 0 and self.board[i][j] == self.board[i+1][j - 1] == self.board[i+2][j - 2] == self.board[i+3][j - 3]:
                         return self.board[i][j] + 1
                 except IndexError:
                     pass
         if self.isFull():
-            return 0
-        return -1
+            return 0  # Match nul si le plateau est plein
+        return -1  # Partie non terminée
 
-    # Returns a unique decimal number for each board object based on the
-    # underlying data
+
+    # Calcule un nombre unique (hash) représentant l'état du plateau
     def hash(self):
-
         power = 0
         hash = 0
-
         for column in self.board:
-
-            # add 0 or 1 depending on piece
+            # Ajoute chaque pion (0 ou 1) dans le hash
             for piece in column:
                 hash += piece * (3 ** power)
                 power += 1
-
-            # add a 2 to indicate end of column
+            # Ajoute un séparateur (2) pour indiquer la fin de colonne
             hash += 2 * (3 ** power)
             power += 1
-
         return hash
 
     ########################################################################
     #                           Utilities
     ########################################################################
 
-    # Return true iff the game is full
+
+    # Retourne True si le plateau est plein (42 coups joués)
     def isFull(self):
         return self.numMoves == 42
 
-    # Prints out a visual representation of the board
-    # X's are 1's and 0's are 0s
+
+    # Affiche une représentation visuelle du plateau dans la console
+    # Les X représentent les 1, les O représentent les 0
     def print(self):
         print("")
         print("+" + "---+" * self.WIDTH)
@@ -165,8 +175,8 @@ class Board(object):
                     row += "   |"
             print(row)
             print("+" + "---+" * self.WIDTH)
-        print(self.lastMove[1])
-        print(self.numMoves)
+        print(self.lastMove[1])  # Affiche la colonne du dernier coup
+        print(self.numMoves)     # Affiche le nombre total de coups
 
 
 
